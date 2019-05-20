2b200ee3071a038d0ea89345b9f9401fa4a07a91
tomsiwek <50829081+tomsiwek@users.noreply.github.com>
2019-05-20 14:01:47 +0200

    Update run_gprmax.bat

--- run_gprmax.bat
+++ run_gprmax.bat
@@ -1,7 +1,6 @@
-:: @chcp 852>NUL
 @echo off
-set conda_dir=C:\ProgramData\Miniconda3
-set gprmax_dir="C:\Users\ùapy precz!\Desktop\Praca magisterska\gprmax\gprMax-v.3.1.4"
+set conda_dir="C:\path_to_conda"
+set gprmax_dir="C:\path_to_gprmax"
 call %conda_dir%\Scripts\activate.bat %conda_dir%
 cd %gprmax_dir%
 activate gprMax & python -m gprMax %1 %2 %3 %4 & conda deactivate & pause & exit
