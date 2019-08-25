28778f3ecae15e14fd3b88013ba53b8af69148b0
Tomasz Siwek <50829081+tomsiwek@users.noreply.github.com>
2019-08-25 23:40:42 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -73,6 +73,7 @@ todo_include_todos = True
 master_doc = 'index'
 
 from sphinx.apidoc import main
+import re
 """Run command."""
 sys.argv[0] = re.sub(r'(-script\.pyw|\.exe)?$', '', sys.argv[0])
 exit = main(['sphinx-apidoc', '-f', '-o', './docs', './lib'])
